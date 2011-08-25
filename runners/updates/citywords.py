ed92ad50fbcad0067fa087e3cb38f32c23c55588
Vinny Murphy <murphy.vinny@gmail.com>
2011-08-24 22:15:09 -0400

    fix the copyright.  emacs put in my Cisco copyright by mistake. :-( modified:   citywords.py

--- runners/updates/citywords.py
+++ runners/updates/citywords.py
@@ -5,14 +5,34 @@
 ######################################################################
 File: urlwords.py
 
-Copyright (c) 2011 by Cisco Systems, Inc.
-All rights reserved.
-Author: Vinny Murphy
+########################################################################
+# Copyright (c) 2011 by Vinny Murphy
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation files
+# (the "Software"), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge,
+# publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+# BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+########################################################################
 
 Date Created: Sun Aug 14 19:10:47 2011
 
-Description:
-<insert file description here!>
+Description: look up the cities in the database for runners and for
+the urls in the new england states and give a list of how many hits
+total for the cities.
 ######################################################################
 '''
 import os
